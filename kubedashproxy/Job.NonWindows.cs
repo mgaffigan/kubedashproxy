bc65d6699e9c0f632efa02c3d32cfd024906f85b
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2024-05-24 18:03:13 -0500

    Allow build on linux/macos

---
+++ kubedashproxy/Job.NonWindows.cs
@@ -0,0 +1,20 @@
+﻿#if !WINDOWS
+
+using System;
+
+namespace Esatto.Win32.Processes;
+
+public class Job : IDisposable
+{
+    public void AddProcess(System.Diagnostics.Process proc)
+    {
+        // nop
+    }
+
+    public void Dispose()
+    {
+        // nop
+    }
+}
+
+#endif
